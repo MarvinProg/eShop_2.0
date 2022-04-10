2556226cb39c282695c4138f604c956234f2be6a
Artem Lomakin <Lomakin-as@mail.ru>
2022-04-10 16:47:03 +0300

    add: function get number items in cart

--- public/script.js
+++ public/script.js
@@ -11,4 +11,20 @@ function add_to_cart(id) {
   let x = window.localStorage.getItem(key);
   x = x * 1 + 1;
   window.localStorage.setItem(key, x);
+
+  alert('Items in your' + get_number_items_in_cart());
+}
+
+function get_number_items_in_cart() {
+  let count = 0;
+  for(let item = 0; window.localStorage.length; item++) {
+    let key = window.localStorage.key(item);
+    let value = window.localStorage.getItem(key);
+
+    if (key.indexOf('product_') == 0) {
+      count = count + value * 1;
+    }
+  }
+
+  return count;
 }
