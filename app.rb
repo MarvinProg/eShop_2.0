5dbae17b77f97da4e190ec866c3c0aba937dd04c
Artem Lomakin <Lomakin-as@mail.ru>
2022-04-23 18:01:29 +0300

    add: Parse 'orders input'.

--- app.rb
+++ app.rb
@@ -21,5 +21,26 @@ get '/about_us' do
 end
 
 post '/cart' do 
+	input_orders = params[:orders]
+	@orders = orders_line_parse input_orders
 	erb :cart
+end
+
+def orders_line_parse input_orders 
+  str_1 = input_orders.split(",")
+
+  arr_1 = []
+
+  str_1.each do |s|
+    str_2 = s.split("=")
+    str_3 = str_2[0].split("_")
+
+    id = str_3[1]
+    count = str_2[1]
+
+    arr_2 = [id, count]
+    arr_1.push(arr_2)
+  end
+
+  return arr_1
 end
